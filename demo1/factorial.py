070180d727c667e9bc13b63a408aeac1577d7a5a
bitbytebrute01 <bitbytebrute01@gmail.com>
2026-01-16 19:53:37 +0530

    Initial Commit

---
+++ demo1/factorial.py
@@ -0,0 +1,7 @@
+#factorial of the program
+import sys 
+import math
+
+num = int(sys.argv[1])
+fact = math.factorial(num)
+print(fact)
--- program01.py
+++
@@ -1,6 +0,0 @@
-import sys
-
-miles = float(sys.argv[1])
-kilometers = miles * 1.60934
-
-print("Kilometers:", kilometers)
